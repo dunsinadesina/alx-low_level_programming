c413f2dbd211b1025b54404d6d9faf4c75eeb9eb
dunsinadesina <adesinajesudunsin@gmail.com>
2023-08-10 08:55:07 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is main function
+ *
+ * Return:  This should be always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

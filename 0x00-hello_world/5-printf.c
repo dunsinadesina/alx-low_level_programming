55bc7842497bb7861ef3ee30ca74be86a5c1b872
dunsinadesina <adesinajesudunsin@gmail.com>
2023-08-10 09:12:03 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - This a the main function
  *
- * Return: This always return 0
+ * Return: always 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

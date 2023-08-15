c45e4706852162f1e5d8361f716733911e1359e6
dunsinadesina <adesinajesudunsin@gmail.com>
2023-08-15 11:05:50 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - This is for adding two arguments
+ * @a: This is a parameter
+ * @b: This is a parameter
+ * Return: This is always 0
+ */
+int add(int a, int b)
+{
+	int result;
+
+		result = a + b;
+		return (result);
+}

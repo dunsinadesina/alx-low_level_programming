50b4f6b59a4913ba7a44961cdcecaf4bdaa4bffa
dunsinadesina <adesinajesudunsin@gmail.com>
2023-08-17 05:39:00 -0700

    Collaboration is Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Multiplication
+ * @a: parameter
+ * @b: parameter
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}

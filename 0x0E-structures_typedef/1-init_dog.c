f2700ea6d23296f82bfee8da68612f780f173bc1
dunsinadesina <adesinajesudunsin@gmail.com>
2023-09-11 11:07:05 -0700

    A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - function
+ * @d: argument
+ * @name: argument
+ * @age: argument
+ * @owner: argument
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		int namelength, ownerlength, i;
+
+		namelength = 0;
+		while (name[namelength] != '\0')
+		{
+			namelength++;
+		}
+		d->name = (char *)malloc(namelength + 1);
+
+		ownerlength = 0;
+		while (ownerlength != '\0')
+		{
+			ownerlength++;
+		}
+		d->owner = (char *)malloc(ownerlength + 1);
+		if (d->name == NULL || d->owner == NULL)
+		{
+			printf("Error\n");
+			exit(1);
+		}
+		for (i = 0; i <= namelength; i++)
+		{
+			d->name[i] = name[i];
+		}
+		d->age = age;
+		for (i = 0; i <= ownerlength; i++)
+		{
+			d->owner[i] = owner[i];
+		}
+	}
+	else
+	{
+		printf("Erroe\n");
+		exit(1);
+	}
+}

6bffe33b48bc754cdaaab6502485f3a8da844894
dunsinadesina <jesudunsinadesina@gmail.com>
2023-09-21 12:53:09 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_before_main - function
+ * @void: nothing
+ * Return: void
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,4 +25,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(char *str);
+void print_before_main(void);
+int main();
 #endif /*LISTS_H*/
